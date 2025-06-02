8f45e03938e4fffd24fa51d9d10d4a16fe95c017
wschafer <wschafer@student.42berlin.de>
2025-06-02 15:00:03 +0200

    implement the following conversions: c, s

--- ft_printf.c
+++ ft_printf.c
@@ -13,13 +13,20 @@
 #include <unistd.h>
 #include <stdarg.h>
 
+void	ft_putstr_fd(char *s, int fd);
+static	size_t	get_lenth(long n);
+static	char	*convert(char *str, long n, size_t i, size_t sign);
+
 int	ft_printf(const char *s, ...)
 {
 	va_list	args;
-	int		i;
+	int		i = 0;
+	char	*str;
+	unsigned int	j;
 
-	i = 0;
+
 	va_start(args, s);
+	
 	while (*s)
 	{
 		if (*s == '%')
@@ -27,8 +34,24 @@ int	ft_printf(const char *s, ...)
 			s++;
 			if (*s == 'c')
 			{
-				write(1, &args, 1);
+				i = va_arg(args, int);
+				write(1, &i, 1);
 			}
+			if (*s == 's')
+			{
+				str = va_arg(args, char*);
+				ft_putstr_fd(str, 1);
+			}
+			if (*s == 'p')
+			{
+				j = va_arg(args, unsigned int);
+				get_lenth(j);
+
+			}
+		}
+		else
+		{
+			write(1, s, 1);			
 		}
 		s++;
 	}
@@ -37,7 +60,51 @@ int	ft_printf(const char *s, ...)
 	return(i);
 }
 
+#include <stdio.h>
 int	main()
 {
-	ft_printf("%c", 'a');
+//	char *str = "abcdef";
+
+	//ft_printf("www %s mmm", str);
+	printf("%x\n", 123);
+}
+
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (!s)
+		return ;
+	while (*s)
+	{
+		write(fd, s, 1);
+		s++;
+	}
+}
+static	size_t	get_lenth(long n)
+{
+	size_t	len;
+
+	if (n == 0)
+		return (1);
+	len = 0;
+	while (n > 0)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+static	char	*convert(char *str, unsigned int n, size_t i, size_t flag)
+{
+	str[i] = '\0';
+	while (i--)
+	{
+		char *base = "123456789ABCDEF";
+		str[i] = n % 16 + '0';
+		n = n / 10;
+	}
+	
+	str[0] = '-';
+	return (str);
 }
